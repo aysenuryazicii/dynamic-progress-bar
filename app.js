04df6003c42d9dcda65aab42855f42eb5f1b8136
Ayşenur Yazıcı <aysenur_yzc@icloud.com>
2022-07-29 14:07:44 +0300

    progress bar

---
+++ app.js
@@ -0,0 +1,17 @@
+const progress = document.querySelector(".progress");
+const loading = document.querySelector(".loading");
+
+let i = 0;
+const fakeUploadPerc = [0, 10, 25, 40, 42, 60, 70, 75, 90, 100];
+
+const interval = setInterval(() => {
+  progress.style.width = fakeUploadPerc[i] + "%";
+  loading.innerHTML = fakeUploadPerc[i] + "%";
+  i++;
+
+  if(i == fakeUploadPerc.length) {
+    clearInterval(interval);
+  loading.innerHTML = "Completed"
+
+  }
+}, 1000);
